c65929f360c5862cb9e0f9b8a9792a6475028d8f
Prashant Battu <pbattu2@Prashants-MacBook-Pro.local>
2018-02-28 23:32:03 +0000

    Initial commit

---
+++ src/main/java/com/sync/pb/WbtestApplication.java
@@ -0,0 +1,12 @@
+package com.sync.pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbtestApplication.class, args);
+	}
+}
